472a29306052ae086cd3ccd47726012247f7700e
rinnegard <victor.rinnegard@hotmail.com>
2024-08-19 14:56:08 +0200

    added db model and auth base

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,27 +1,7 @@
-"use client";
-
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
-const PAGES = [
-    { key: crypto.randomUUID(), href: "/", name: "Home" },
-    {
-        key: crypto.randomUUID(),
-        href: "/movies",
-        name: "Movies",
-    },
-    {
-        key: crypto.randomUUID(),
-        href: "/genres",
-        name: "Genres",
-    },
-    {
-        key: crypto.randomUUID(),
-        href: "/people",
-        name: "People",
-    },
-];
+import SignIn from "./sign-in";
+import NavBar from "./navbar";
 
 export default function Header() {
     return (
@@ -29,35 +9,8 @@ export default function Header() {
             <h1 className="text-3xl">
                 <Link href="/">MovieStore</Link>
             </h1>
+            <SignIn></SignIn>
             <NavBar></NavBar>
         </header>
-    );
-}
-
-function NavBar() {
-    const path = usePathname();
-
-    console.log(path);
-
-    return (
-        <nav className="flex justify-center">
-            <ul className="flex gap-2">
-                {PAGES.map((page) => {
-                    return (
-                        <li key={page.key} className="flex items-center">
-                            <Link
-                                className={cn(
-                                    "hover:bg-white hover:text-black hover:rounded-lg p-2",
-                                    path === page.href && "underline"
-                                )}
-                                href={page.href}
-                            >
-                                {page.name}
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
-        </nav>
     );
 }

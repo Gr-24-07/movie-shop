e706faad948b90a747d78c0abea14305dd92de4e
rinnegard <victor.rinnegard@hotmail.com>
2024-08-20 14:01:37 +0200

    admin nav added

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,51 +1,15 @@
-"use client";
+import { auth } from "@/auth";
+import AdminNavBar from "./admin-nav";
+import UserNavBar from "./user-nav";
 
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-const PAGES = [
-    { key: crypto.randomUUID(), href: "/", name: "Home" },
-    {
-        key: crypto.randomUUID(),
-        href: "/admin/movies",
-        name: "Movies",
-    },
-    {
-        key: crypto.randomUUID(),
-        href: "/admin/genres",
-        name: "Genres",
-    },
-    {
-        key: crypto.randomUUID(),
-        href: "/admin/people",
-        name: "People",
-    },
-];
-
-export default function NavBar() {
-    const path = usePathname();
-
-    console.log(path);
+export default async function NavBar() {
+    const session = await auth();
 
     return (
         <nav className="flex justify-center">
             <ul className="flex gap-2">
-                {PAGES.map((page) => {
-                    return (
-                        <li key={page.key} className="flex items-center">
-                            <Link
-                                className={cn(
-                                    "hover:bg-white hover:text-black hover:rounded-lg p-2",
-                                    path === page.href && "underline"
-                                )}
-                                href={page.href}
-                            >
-                                {page.name}
-                            </Link>
-                        </li>
-                    );
-                })}
+                {session?.user.role === "ADMIN" && <AdminNavBar></AdminNavBar>}
+                <UserNavBar></UserNavBar>
             </ul>
         </nav>
     );

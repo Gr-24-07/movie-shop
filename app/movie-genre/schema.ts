4032504372823c0db7e23f4ef6cfafb5319d2d7d
EshaniHansika <eshanihansi@gmail.com>
2024-08-20 08:43:52 +0200

    done

--- app/movie-genre/schema.ts
+++ app/movie-genre/schema.ts
@@ -2,7 +2,7 @@
 import { z } from "zod";
 
 export const AddGenreSchema = z.object({
-    name:      z.string().min(1, "please Enter the Name of the Movie").trim(),
+    name:      z.string().min(1, "please Enter the Name of the Genre").trim(),
     
     
 

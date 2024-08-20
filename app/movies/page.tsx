842078ae8567b28fad81a177c4e8539f0bb90229
89poyjal <epoya001@gmail.com>
2024-08-20 13:09:41 +0200

    addmovie

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,38 +1,58 @@
 "use client";
-import {useState} from 'react';
+
+import { addmovie } from "../actions/movies";
+
 
 
 
 export default function AddMovie() {
-    const [title, setTitle] = useState("");
-    const [director, setDirector] = useState("");
-    const [year, setYear] = useState("");
     
     return (
         <div className="flex justify-center">
-        <form className='flex flex-col w-1/2 gap-4'>
+        <form action={addmovie} className="flex flex-col w-1/2 gap-4">
             
             <input className="text-left font-bold bg-slate-100"
             type="text"
-            value={title}
-            onChange={(e)=> setTitle(e.target.value)}
             placeholder="Movie Title"
             required
+            name="title"
             />
             <input className="text-left font-bold bg-slate-100"
             type="text"
-            value={director}
-            onChange={(e)=> setDirector(e.target.value)}
-            placeholder="Director"
+            placeholder="Movie Description"
+            name="description"
+           
             required
             />
             <input className="text-left font-bold bg-slate-100"
             type="number"
-            value={year}
-            onChange={(e)=> setYear(e.target.value)}
-            placeholder="Year"
+            placeholder="Movie Price"
+            name="price"
             required
             />
+
+           <input className="text-left font-bold bg-slate-100"
+            type="date"
+            placeholder="Movie releaseDate"
+            name="releaseDate"
+            required
+            />
+
+          <input className="text-left font-bold bg-slate-100"
+            type="number"
+            placeholder="Movie Stock"
+            name="stock"
+            required
+            />
+
+         <input className="text-left font-bold bg-slate-100"
+            type="string"
+            placeholder="Movie imageURL"
+            name="imageURL"
+            required
+            />
+
+
             <button className="p-2 bg-blue-600 text-white rounded" >
                 Add Movie
                 </button>

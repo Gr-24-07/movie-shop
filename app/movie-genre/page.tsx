a71ea0fe34f9d7a98aaa9ecdb18abf0ea4a9e14c
EshaniHansika <eshanihansi@gmail.com>
2024-08-20 08:39:33 +0200

    maked genre

--- app/movie-genre/page.tsx
+++ app/movie-genre/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import AddButton from "../components/genre-addbutton";
+import { AddGenreAction, AddGenreFailure } from "./action";
+import FormError from "../components/form-error";
 
-export default function MovieGenre() {
-    const [title, setTitle] = useState("");
-    const [genre, setGenre] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log("Title:", title);
-        console.log("Genre:", genre);
-    };
+export default function AddGenrePage () {
+    const formRef = useRef<HTMLFormElement>(null);
+    const [errors, setErrors] = useState<AddGenreFailure["errors"] | undefined>();
+
+    async function action (formData: FormData) {
+        const result = await AddGenreAction(formData);
+
+        if (result.success) {
+          formRef.current?.reset();  
+          setErrors(undefined);
+          return;
+        }
+        else{
+            setErrors(result.errors);
+        }  
+    }
+
 
     return (
         <main className="container mx-auto my-12">
@@ -20,36 +31,21 @@ export default function MovieGenre() {
                 <h1 className="text-center">Movie Genre</h1>
             </div>
 
-            <form className=" p-6  mt-6 space-y-6" onSubmit={handleSubmit}>
+            <form className=" p-6  mt-6 space-y-6" action={action}>
                 <div className="flex flex-col">
                     <label htmlFor="title" className="text-gray-700 font-medium mb-2">
-                        Title:
+                        Name:
                     </label>
                     <input
                         type="text"
-                        id="title"
-                        name="title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        id="name"
+                        name="name"
                         className="border border-gray-400 p-2 rounded focus:outline-none focus:border-blue-500"
-                        required
                     />
+                    <FormError errors={errors?.name?._errors}/>
                 </div>
 
-                <div className="flex flex-col">
-                    <label htmlFor="genre" className="text-gray-700 font-medium mb-2">
-                        Genre Name:
-                    </label>
-                    <input
-                        type="text"
-                        id="genre"
-                        name="genre"
-                        value={genre}
-                        onChange={(e) => setGenre(e.target.value)}
-                        className="border border-gray-400 p-2 rounded focus:outline-none focus:border-blue-500"
-                        required
-                    />
-                </div>
+               
 
                 <AddButton />
             </form>

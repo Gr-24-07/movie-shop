9f37609b0e886903f9db076b73222874cddce6d0
89poyjal <epoya001@gmail.com>
2024-08-21 11:22:34 +0200

    delete movie

--- app/actions/movies.ts
+++ app/actions/movies.ts
@@ -1,49 +1,64 @@
-'use server'
+"use server";
 import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { bigint, string, z } from "zod";
 
 const movievalidation = z.object({
-        title: z.string(),
-        description: z.string(),
-        price: z.coerce.number().int(),
-        releaseDate:z.coerce.date(),
-        stock: z.coerce.number(),
-        imageURL: z.string(),
-
+  title: z.string(),
+  description: z.string(),
+  price: z.coerce.number().int(),
+  releaseDate: z.coerce.date(),
+  stock: z.coerce.number(),
+  imageURL: z.string(),
 });
 
 export async function addmovie(formData: FormData) {
-    const data = Object.fromEntries(formData.entries());
-    let validatedData;
+  const data = Object.fromEntries(formData.entries());
+  let validatedData;
 
-    try {
-        validatedData = await movievalidation.parseAsync(data);
-    } catch (error) {
-        console.error('Validation error:', error);
-        return;
-    }
+  try {
+    validatedData = await movievalidation.parseAsync(data);
+  } catch (error) {
+    console.error("Validation error:", error);
+    return;
+  }
 
-    if (!validatedData) {
-        console.error("No validated data found");
-        return;
-    }
+  if (!validatedData) {
+    console.error("No validated data found");
+    return;
+  }
 
-    try {
-        await prisma.movie.create({
-            data: {
-                title: validatedData.title,
-                description: validatedData.description,
-                price: validatedData.price,
-                releaseDate: validatedData.releaseDate,
-                stock: validatedData.stock,
-                imageURL: validatedData.imageURL,
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return;
-    }
-    revalidatePath("./movie");
+  try {
+    await prisma.movie.create({
+      data: {
+        title: validatedData.title,
+        description: validatedData.description,
+        price: validatedData.price,
+        releaseDate: validatedData.releaseDate,
+        stock: validatedData.stock,
+        imageURL: validatedData.imageURL,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+  revalidatePath("./movie");
+}
+
+export default async function DeleteMovie(id: string) {
+  try {
+    await prisma.movie.delete({
+      where: {
+        id: id,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  // redirect("/");
+  revalidatePath("/admin/movies");
 }

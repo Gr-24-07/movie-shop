f35cbb7ed16593fc268b2f8687aa04b126726057
EshaniHansika <eshanihansi@gmail.com>
2024-08-21 14:34:54 +0200

    Genre List done

--- app/admin/genres/page.tsx
+++ app/admin/genres/page.tsx
@@ -4,12 +4,11 @@ import React, { useRef, useState } from "react";
 import { AddGenreAction, AddGenreFailure } from "./action";
 import AddButton from "@/app/components/genre-addbutton";
 import FormError from "@/app/components/form-error";
+import GenreListWrapper from "@/app/components/GenreListWrapper";
 
 export default function AddGenrePage() {
     const formRef = useRef<HTMLFormElement>(null);
-    const [errors, setErrors] = useState<
-        AddGenreFailure["errors"] | undefined
-    >();
+    const [errors, setErrors] = useState<AddGenreFailure["errors"] | undefined>();
 
     async function action(formData: FormData) {
         const result = await AddGenreAction(formData);
@@ -25,11 +24,11 @@ export default function AddGenrePage() {
 
     return (
         <main className="container mx-auto my-12">
-            <div className=" bg-blue-500 p-4 rounded-lg font-bold">
+            <div className="bg-blue-500 p-4 rounded-lg font-bold">
                 <h1 className="text-center">Movie Genre</h1>
             </div>
 
-            <form className=" p-6  mt-6 space-y-6" action={action}>
+            <form ref={formRef} className="p-6 mt-6 space-y-6" action={action}>
                 <div className="flex flex-col">
                     <label
                         htmlFor="title"
@@ -48,6 +47,8 @@ export default function AddGenrePage() {
 
                 <AddButton />
             </form>
+
+            <GenreListWrapper />
         </main>
     );
 }

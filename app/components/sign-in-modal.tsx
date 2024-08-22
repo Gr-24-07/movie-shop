8dd12aed5d70f7c088656b057f8008735d85d2d1
rinnegard <victor.rinnegard@hotmail.com>
2024-08-22 16:14:51 +0200

    fixed event propogation

--- app/components/sign-in-modal.tsx
+++ app/components/sign-in-modal.tsx
@@ -14,13 +14,24 @@ export default function SignInModal({
             className="bg-black bg-opacity-50 w-full h-full absolute top-0 left-0
         flex justify-center items-center"
         >
-            <div className="bg-white p-4 h-72 w-56 rounded-md flex flex-col justify-between items-center gap-2">
+            <div
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+                className="bg-white p-4 h-72 w-56 rounded-md flex flex-col justify-between items-center gap-2"
+            >
                 <div className="flex flex-col gap-2">
                     <h1 className="font-bold text-center">Sign in</h1>
                     <SignIn provider="google" Icon={FaGoogle}></SignIn>
                     <SignIn provider="github" Icon={FaGithub}></SignIn>
                 </div>
-                <Button onClick={handleClose}>Close</Button>
+                <Button
+                    onClick={(e) => {
+                        handleClose();
+                    }}
+                >
+                    Close
+                </Button>
             </div>
         </div>,
         document.body

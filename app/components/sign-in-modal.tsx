95efd21493aedbea3dd640c4374fdc8a9d5cad68
rinnegard <victor.rinnegard@hotmail.com>
2024-08-22 16:04:09 +0200

    Update sign-in-modal.tsx
    Fixed style and added close modal when clicking outside

--- app/components/sign-in-modal.tsx
+++ app/components/sign-in-modal.tsx
@@ -10,10 +10,11 @@ export default function SignInModal({
 }) {
     return createPortal(
         <div
+            onClick={handleClose}
             className="bg-black bg-opacity-50 w-full h-full absolute top-0 left-0
         flex justify-center items-center"
         >
-            <div className="bg-white p-4 h-1/3 w-2/6 rounded-md flex flex-col justify-between items-center gap-2">
+            <div className="bg-white p-4 h-72 w-56 rounded-md flex flex-col justify-between items-center gap-2">
                 <div className="flex flex-col gap-2">
                     <h1 className="font-bold text-center">Sign in</h1>
                     <SignIn provider="google" Icon={FaGoogle}></SignIn>

bb7e7c84d9c8a221ece4f94f8e83b7c51f18de08
89poyjal <epoya001@gmail.com>
2024-08-26 15:07:05 +0200

    movie details

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import prisma from "@/lib/db";
+import Link from 'next/link';
 
  
 export default async function Page() {
@@ -20,11 +21,13 @@ export default async function Page() {
             <Image  
             src={movie.imageURL || "http://localhost:3000/_next/image?url=https%3A%2F%2Fwww.themoviedb.org%2Ft%2Fp%2Fw600_and_h900_bestv2%2Fb33nnKl1GSFbao4l3fZDDqsMx0F.jpg&w=640&q=75"}
             alt="Picture of the movie"
-            width={200}
+            width={300}
             height={50}
-
+            
            
             />
+            <Link href={`/movies/${movie.id}`}>Link
+            </Link>
              <button className="p-2 text-justify font-bold bg-green-500 px-2 py-3 rounded-lg">
                 Buy Movie
              </button>

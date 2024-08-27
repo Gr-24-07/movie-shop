f5ce5d91f1cb48c7e276552164d845c122720cb6
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-08-27 08:44:13 +0200

    Fixed cart size only showing when there is something in cart

--- app/components/cart-icon.tsx
+++ app/components/cart-icon.tsx
@@ -3,9 +3,12 @@ import { ShoppingCart } from "lucide-react";
 export default function CartIcon({ cartSize }: { cartSize: number }) {
     return (
         <div className="relative">
-            <div className="absolute w-6 top-3 left-3 bg-red-700 rounded-full flex justify-center">
-                {cartSize}
-            </div>
+            {cartSize > 0 && (
+                <div className="absolute w-6 top-3 left-3 bg-red-700 rounded-full flex justify-center">
+                    {cartSize}
+                </div>
+            )}
+
             <ShoppingCart />
         </div>
     );

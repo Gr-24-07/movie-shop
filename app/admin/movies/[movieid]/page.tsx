8d35b68b21707fafdd9e4c3ad1f709d587996784
89poyjal <epoya001@gmail.com>
2024-08-28 15:11:57 +0200

    work with update

--- app/admin/movies/[movieid]/page.tsx
+++ app/admin/movies/[movieid]/page.tsx
@@ -2,6 +2,7 @@
 
 import  { updateMovie } from "@/app/actions/movies";
 import prisma from "@/lib/db";
+import { currencyFormatter } from "@/lib/formats";
 import { useRouter } from 'next/router'
 
 
@@ -22,77 +23,102 @@ export default async function UpdateMovie({params}: { params: {
   
   return (
     <>
-    <div className="flex justify-center my-4 shadow-md rounded-lg overflow-hidden bg-slate-300 min-w-96 text-center">
+    <div className="flex justify-center my-4 shadow-md rounded-lg overflow-hidden bg-white min-w-96 text-center">
 
-      <form >
-      
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="text"
-          placeholder="Movie Title"
-          defaultValue={movie?.title}
-          
-          required
-          name="title"
-        />
-
-
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="text"
-          placeholder="Movie Description"
-          defaultValue={movie?.description || ""}
-          name="description"
-          required
-        />
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="number"
-          placeholder="Movie Price"
-          defaultValue={movie?.price.toString() || ""}
-          name="price"
-          required
-        />
-
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="date"
-          placeholder="Movie releaseDate"
-          defaultValue={movie?.releaseDate.toDateString() || ""}
-          name="releaseDate"
-          required
-        />
-
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="number"
-          placeholder="Movie Stock"
-          defaultValue={movie?.stock}
-          name="stock"
-          required
-        />
-
-        <input
-          className="text-left font-bold bg-slate-100"
-          type="string"
-          placeholder="Movie imageURL"
-          defaultValue={movie?.imageURL || ""}
-          name="URL"
-
-          required
-        />
-        
-        <button type="submit"
-         formAction={async (formData) => {
+        <form className="flex flex-col gap-4 w-96" action={async (formData) => {
                     "use server";
                     await updateMovie(formData, id);
         
                     
-         }} className="p-2 bg-blue-600 text-white rounded-lg">
-           Update Movie  
-         </button> 
-        
-         </form>
+         }} method="POST">
+            <h2 className="text-2xl font-bold text-center mb-4">Update Movie</h2>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="title">
+              Title
+            </label>
+            <input
+              className="p-2 rounded-md border border-gray-300"
+              type="text"
+              id="title"
+              placeholder="Movie Title"
+              defaultValue={movie?.title}
+              required
+              name="title"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="description">
+              Description
+            </label>
+            <textarea
+              className="p-2 rounded-md border border-gray-300 resize-none"
+              id="description"
+              placeholder="Movie Description"
+              defaultValue={movie?.description || ''}
+              required
+              name="description"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="price">
+              Price
+            </label>
+            <input
+              className="p-2 rounded-md border border-gray-300"
+              type="number"
+              id="price"
+              placeholder="Movie Price"
+              defaultValue={currencyFormatter.format(Number(movie?.price))}
+              required
+              name="price"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="releaseDate">
+              Release Date
+            </label>
+            <input
+              className="p-2 rounded-md border border-gray-300"
+              type="date"
+              id="releaseDate"
+              placeholder="Movie releaseDate"
+              defaultValue={movie?.releaseDate.toISOString().split('T')[0]}
+              required
+              name="releaseDate"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="stock">
+              Stock
+            </label>
+            <input
+              className="p-2 rounded-md border border-gray-300"
+              type="number"
+              id="stock"
+              placeholder="Movie Stock"
+              defaultValue={movie?.stock}
+              required
+              name="stock"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-semibold" htmlFor="URL">
+              Image URL
+            </label>
+            <input
+              className="p-2 rounded-md border border-gray-300"
+              type="text"
+              id="URL"
+              placeholder="Movie imageURL"
+              defaultValue={movie?.imageURL || ""}
+              required
+              name="URL"
+            />
+          </div>
+          <button type="submit" className="p-2 bg-blue-600 text-white rounded-lg">
+             Update Movie
+           </button> 
+        </form>
          </div>
          </>
   )};

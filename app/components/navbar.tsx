4cacc52c0daa78e1df494c996cad70a3d9b9400f
rinnegard <57401904+rinnegard@users.noreply.github.com>
2024-09-05 11:20:00 +0200

    updated sheet to close when movie link is clicked

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,14 @@ import { auth } from "@/auth";
 import AdminNavBar from "./admin-nav";
 import UserNavBar from "./user-nav";
 import { getCartSize } from "../actions/cart";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import {
+    Sheet,
+    SheetTrigger,
+    SheetContent,
+    SheetClose,
+    SheetTitle,
+    SheetDescription,
+} from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import CartNavBar from "./cart-navbar";
@@ -29,9 +36,13 @@ export default async function NavBar() {
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left" className="flex flex-col gap-4">
+                        <SheetDescription className="sr-only">
+                            Navigation links
+                        </SheetDescription>
+                        <SheetTitle className="sr-only">Navigation</SheetTitle>
                         <ul className="flex gap-2">
                             {session?.user.role === "ADMIN" && <AdminNavBar />}
-                            <UserNavBar />
+                            <UserNavBar isSheet={true} />
                         </ul>
                     </SheetContent>
                 </Sheet>

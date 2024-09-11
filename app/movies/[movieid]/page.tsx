25091b83f24ec52b909ce617706409c4d6f34497
89poyjal <epoya001@gmail.com>
2024-09-11 13:57:08 +0200

    work with movie details design

--- app/movies/[movieid]/page.tsx
+++ app/movies/[movieid]/page.tsx
@@ -29,80 +29,100 @@ export default async function DetailsMovie({
   if (!movie) return notFound();
 
   return (
-    <div className="flex flex-col items-center p-2 rounded-lg shadow-lg bg-white">
+    <div className="container mx-auto flex flex-col items-center bg-white p-4 rounded-lg shadow-lg">
       <h1 className="text-3xl font-bold text-center">{movie?.title}</h1>
 
-      <div className="flex items-center justify-center w-80 py-4">
-        <p className="text-lg font-semibold border border-spacing-1 border-black">
-          {movie?.description}
-        </p>
-      </div>
+      <div className="flex flex-col gap-2 py-4">
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-semibold border-b border-gray-300">
+            Description
+          </p>
+          <p className="text-justify">{movie?.description}</p>
+        </div>
 
-      <div className="flex items-center justify-center">
-        <div className="flex items-center">
-          <p className="text-3xl font-bold text-red-500 border border-spacing-1 border-black">
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-semibold border-b border-gray-300">
+            Price
+          </p>
+          <p className="text-3xl font-bold text-red-500">
             {currencyFormatter.format(Number(movie?.price))}
           </p>
-          <p className="text-lg font-semibold text-gray-500 ml-4 border-spacing-1 border-slate-100">
-            ReleaseDate: {movie?.releaseDate.toLocaleDateString()}
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-semibold border-b border-gray-300">
+            Release Date
+          </p>
+          <p className="text-lg font-semibold text-gray-500">
+            {movie?.releaseDate.toLocaleDateString()}
           </p>
         </div>
-      </div>
-      <div className="flex items-center justify-center">
-        <Image
-          src={movie?.imageURL || ""}
-          alt="Picture of the movie"
-          width={200}
-          height={300}
-          className="rounded-lg"
-        />
-      </div>
-      <div className="flex flex-grow-4 my-4 rounded-lg  bg-slate-300 min-w-96 text-center">
-        <h1 className="text-center font-extrabold border-separate border-black border-2">
-          Genres
-        </h1>
-        <ul className="flex flex-wrap gap-2">
-          {movie?.genres.map((genre) => (
-            <li
-              className="flex flex-col place-items-center rounded-lg bg-slate-200 p-2"
-              key={genre.id}
-            >
-              {genre.name}
-            </li>
-          ))}
-          {movie?.peopleJob.map((peoplejob) => (
-            <li
-              className="flex flex-col place-items-center rounded-lg bg-slate-200 p-2"
-              key={peoplejob.id}
-            >
-              <p className="text-center text-lg font-bold">
-                {peoplejob.jobTitle}
-              </p>
-              <p className="text-center text-sm">{peoplejob.people.name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex items-center justify-center">
-        <p className="text-2xl font-bold text-center text-cyan-600">
-          Stock: {movie?.stock}
-        </p>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-semibold border-b border-gray-300">
+            Genres
+          </p>
+          <ul className="flex flex-wrap gap-2">
+            {movie?.genres.map((genre) => (
+              <li
+                className="flex flex-col place-items-center rounded-lg bg-slate-200 p-2"
+                key={genre.id}
+              >
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-semibold border-b border-gray-300">
+            People
+          </p>
+          <ul className="flex flex-wrap gap-2">
+            {movie?.peopleJob.map((peoplejob) => (
+              <li
+                className="flex flex-col place-items-center rounded-lg bg-slate-200 p-2"
+                key={peoplejob.id}
+              >
+                <p className="text-center text-lg font-bold">
+                  {peoplejob.jobTitle}
+                </p>
+                <p className="text-center text-sm">{peoplejob.people.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
-      <form
-        action={async () => {
-          "use server";
-          await addToCart({
-            id: movie.id,
-            price: movie.price.toNumber(),
-            title: movie.title,
-          });
-        }}
-      >
-        <button className="w-full p-2 text-justify font-bold bg-green-500 px-2 py-3 rounded-sm hover:bg-green-600 active:bg-green-700">
-          Add To Cart
-        </button>
-      </form>
+      <div className="flex flex-col items-start p-4 w-full">
+        <p className="text-3xl font-bold text-center text-cyan-600 border-spacing-2 bordeer-black">
+          Stock: {movie?.stock}
+        </p>
+        <div className="flex flex-col gap-2 ">
+          <Image
+            src={movie?.imageURL || ""}
+            alt="Picture of the movie"
+            width={200}
+            height={100}
+            className="rounded-lg shadow-md "
+          />
+        </div>
+
+        <form
+          action={async () => {
+            "use server";
+            await addToCart({
+              id: movie.id,
+              price: movie.price.toNumber(),
+              title: movie.title,
+            });
+          }}
+        >
+          <button className="w-full p-2 text-center font-bold bg-green-500 rounded-lg hover:bg-green-600 active:bg-green-700">
+            Add To Cart
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
